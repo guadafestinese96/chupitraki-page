8f71410506aa2cc52c872941ba29d877b13a1d08
guadafestinese <guadalupefestinese@gmail.com>
2024-08-09 14:42:52 -0300

    dd

--- src/Components/Item/ItemsBebidasWisky.js
+++ src/Components/Item/ItemsBebidasWisky.js
@@ -1,35 +1,35 @@
-import goldLabel from '../../assets/goldLabel.png';
+
 
 export default [
     {
         id: 1,
         marca: "johnnie walker",
         modelo: "gold label",
-        img: {goldLabel},
+        img: '../../src/assets/goldLabel.png',
     },{
         id: 2,
         marca: "johnnie walker",
         modelo: "blue label",
-        img: "/src/assets/blueLabel.png",
+        img: "../../src/assets/blueLabel.png",
     },{
         id: 3,
         marca: "johnnie walker",
         modelo: "red label",
-        img: "/src/assets/redLabel.png",
+        img: "../../src/assets/redLabel.png",
     },{
         id: 4,
         marca: "johnnie walker",
         modelo: "black label",
-        img: "/src/assets/blackLabel.png",
+        img: "../../src/assets/blackLabel.png",
     },{
         id: 5,
         marca: "chivas",
         modelo: "12 años",
-        img: "/src/assets/chivas12.png",
+        img: "../../src/assets/chivas12.png",
     },{
         id: 6,
         marca: "chivas",
         modelo: "18 años",
-        img: "/src/assets/chivas18.png",
+        img: "../../src/assets/chivas18.png",
     }
 ]
--- src/Components/Item/itemsBebidasVodka.js
+++ src/Components/Item/itemsBebidasVodka.js
@@ -4,30 +4,30 @@ export default [
         id: 1,
         marca: "belvedere",
         modelo: "luminous",
-        img: "/src/assets/belvedereLuminous.jpg"
+        img: "../../src/assets/belvedereLuminous.jpg"
     },
     {
         id: 2,
         marca: "belvedere",
         modelo: "classic",
-        img: "/src/assets/belvedere.png"
+        img: "../../src/assets/belvedere.png"
     },
     {
         id: 3,
         marca: "belvedere",
         modelo: "magnum",
-        img: "/src/assets/belvedereMagnum.png"
+        img: "../../src/assets/belvedereMagnum.png"
     },
     {
         id: 4,
         marca: "grey goose",
         modelo: "classic",
-        img: "/src/assets/greyGoose.png"
+        img: "../../src/assets/greyGoose.png"
     },
     {
         id: 5,
         marca: "grey goose",
         modelo: "strawberry & lemongrass",
-        img: "/src/assets/greyGooseStrawberry.png"
+        img: "../../src/assets/greyGooseStrawberry.png"
     }
 ]
