118c03c253ea14fbc09ac6e935324b8e38128002
guadafestinese <guadalupefestinese@gmail.com>
2024-08-11 15:25:36 -0300

    img

--- src/Components/Item/ItemsBebidasChampagne.js
+++ src/Components/Item/ItemsBebidasChampagne.js
@@ -13,41 +13,41 @@ export default [
         id: 3,
         marca: "moet",
         modelo: "rose imperial",
-        img: "../../src/assets/moetRoseImperial.png"
+        img: "/src/assets/moetRoseImperial.png"
     },{
         id: 4,
         marca: "moet",
         modelo: "nectar",
-        img: "../../src/assets/moetNectar.png"
+        img: "/src/assets/moetNectar.png"
     },{
         id: 5,
         marca: "clicquot",
         modelo: "rich",
-        img: "../../src/assets/rich.png"
+        img: "/src/assets/rich.png"
     },{
         id: 6,
         marca: "clicquot",
         modelo: "brut",
-        img: "../../src/assets/clicquotBrut.png"
+        img: "/src/assets/clicquotBrut.png"
     },{
         id: 7,
         marca: "clicquot",
         modelo: "rose",
-        img: "../../src/assets/clicquotRose.png"
+        img: "/src/assets/clicquotRose.png"
     },{
         id: 8,
         marca: "chandon",
         modelo: "delice",
-        img: "../../src/assets/delice.png"
+        img: "/src/assets/delice.png"
     },{
         id: 9,
         marca: "chandon",
         modelo: "rose",
-        img: "../../src/assets/chandonRose.png"
+        img: "/src/assets/chandonRose.png"
     },{
         id: 10,
         marca: "chandon",
         modelo: "extra brut",
-        img: "../../src/assets/chandonExtraBrut.png"
+        img: "/src/assets/chandonExtraBrut.png"
     }
 ]
