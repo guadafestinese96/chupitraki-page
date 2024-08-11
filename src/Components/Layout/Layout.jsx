d3514e699702186b0df8da7f2ba71469d57360d7
guadafestinese <guadalupefestinese@gmail.com>
2024-08-11 14:48:29 -0300

    chupitraki

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -2,12 +2,15 @@ import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
 import './Layout.css'
 
-export default function Layout({children}){
-    return(
-        <div className="divLayout"> 
-            <Header/>
+export default function Layout({ children }) {
+    return (
+        <div className="divLayout">
+            <Header />
+            <div className="children">
                 {children}
-            <Footer/>
+            </div>
+
+            <Footer />
         </div>
     )
 }
