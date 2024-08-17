5408ce0371f7263c3dda23da911ed7e5b4aaff2c
guadafestinese <guadalupefestinese@gmail.com>
2024-08-17 20:19:10 -0300

    22

--- src/Components/Header/Dropdown.jsx
+++ src/Components/Header/Dropdown.jsx
@@ -3,15 +3,15 @@ import copa from '../../assets/coctail.png'
 
 export default function Example() {
     return (
-        <div class="dropdown">
+        <div className="dropdown">
             <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <img src={copa} className="copa" alt="copa"/>
                 
             </button>
-            <ul class="dropdown-menu">
-            <li><NavLink to='/wisky'><a className="dropdown-item" href="#">wisky</a></NavLink></li>
-                <li><NavLink to='/champagne'><a className="dropdown-item" href="#">champagne</a></NavLink></li>
-                <li><NavLink to='/vodka'><a className="dropdown-item" href="#">vodka</a></NavLink></li>
+            <ul className="dropdown-menu">
+            <li className="liDropdown"><NavLink to='/wisky'><a className="dropdown-item" href="#">wisky</a></NavLink></li>
+                <li className="liDropdown"><NavLink to='/champagne'><a className="dropdown-item" href="#">champagne</a></NavLink></li>
+                <li className="liDropdown"><NavLink to='/vodka'><a className="dropdown-item" href="#">vodka</a></NavLink></li>
                 
             </ul>
         </div> 
