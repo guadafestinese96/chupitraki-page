196c845b21445e0b4636ffe669e89aeeb03ba9c3
guadafestinese <guadalupefestinese@gmail.com>
2024-08-17 20:55:25 -0300

    .

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -1,6 +1,4 @@
 import './Inicio.css'
-import bebidasImportadas from '../../assets/coctail.png'
-
 
 
 export default function Inicio() {
@@ -8,8 +6,7 @@ export default function Inicio() {
     return (
         <div className="inicio">
             <h1 className="titleIndex">DISTRIBUIDORA CHUPITRAKI</h1>
-            {/* <h2 className="titleIndex2">Encuentra las mejores bebidas nacionales e importadas en nuestra página</h2> */}
-            <img src={bebidasImportadas} alt="bebidasInicio" className='bebidasInicio' />
+            
 
         </div>
     )
