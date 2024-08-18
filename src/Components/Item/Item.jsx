48881cf5a34745db95f0edb3bf92a8c03f97cff8
guadafestinese <guadalupefestinese@gmail.com>
2024-08-18 02:11:03 -0300

    .

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -3,9 +3,12 @@ import "./Item.css"
 export default function Item({ item }) {
     return (
         <div className="itemContainer">
-            <div className="itemImgContainer"><img className="itemImg" src={item.img} alt={item.modelo} /></div>
-            <p className="itemMarca">{item.marca}</p>
-            <p className="itemModelo">{item.modelo}</p>
+            <div className="itemImgContainer"><img className="itemImg" src={item.img} alt={item.modelo} />
+            </div>
+            <div className="itemDetails">
+                <h3 className="itemMarca">{item.marca}</h3>
+                <h3 className="itemModelo">{item.modelo}</h3>
+            </div>
         </div>
     )
 }
