5e860e40e9bb8deee7b9bf0b1c8fbf6ee624145c
guadafestinese <guadalupefestinese@gmail.com>
2024-10-10 14:19:14 -0300

    .

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -15,11 +15,18 @@ align-items: center;
 export default function Cart(){
     const {cart} = useContext(CartContext);
 
+    // const cartTotal = cart.reduce((acc, item)=>{
+    //     return item.quantity*item.precio
+    // },0)
+
     return(
+        <div className="flex flex-col justify-center items-center">
         <CartContainer>
             {cart.map((item, index)=>(
             <CartItem item={item} key={index}/> ))
         }
         </CartContainer>
+        <p className="bg-black text-white text-center w-[150px]">Total: $</p>
+        </div>
     )
 }
